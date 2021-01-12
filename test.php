c142c9e7802267078e25ac073cf08c6a45df853d
Stephan Vock <stephan.vock@gmail.com>
2021-01-12 15:08:43 +0000

    files

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
 
 for ($i = 0; $i < 100; $i++) {
-	file_put_contents('./testa' . $i . '.txt', bin2hex(random_bytes(20000)));
+	file_put_contents('./testab' . $i . '.txt', bin2hex(random_bytes(20000)));
 }
